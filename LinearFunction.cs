59c5f5be5e98cda49a5c13366a409c62e55d70b1
Ilya Markov <imarkovkin@gmail.com>
2022-11-21 23:33:14 +0300

    Добавьте файлы проекта.

---
+++ LinearFunction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11
+{
+    struct LinearFunction
+    {
+        double k;
+        double b;
+
+        public LinearFunction (double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            double x = -b / k;
+            return $"Корень уравнения вида kx+b=0: {x}"; 
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите коэффициент k");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите коэффициент b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            LinearFunction kxb = new LinearFunction(k, b);
+            Console.WriteLine(kxb.Root());
+            Console.ReadKey();
+        }
+    }
+}
